98ff035c9271d24ad43a08635f931574a00aa52b
Odionyebuchukwu Jude <82416328+jayjaychukwu@users.noreply.github.com>
2022-06-10 19:21:46 +0100

    requirements.txt

--- magicmethods.py
+++ magicmethods.py
@@ -1,3 +1,13 @@
+"""
+Dunder or magic methods in Python are the methods having two prefix and suffix
+underscores in the method name.
+Dunder here means “Double Under (Underscores)”.
+These are commonly used for operator overloading. 
+These methods are the reason we can add two strings with '+' operator
+without any explicit typecasting.
+
+Here are examples below
+"""
 class stringElement():
 
     #initializing the string
