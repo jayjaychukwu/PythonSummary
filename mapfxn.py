a6cdbebf85882b45f567cd75219a22535570921b
jayjaychukwu <liljaydon.pj@gmail.com>
2022-06-21 09:43:41 +0100

    map function

--- mapfxn.py
+++ mapfxn.py
@@ -4,3 +4,14 @@ calling a function for every array element. It calls a function once
 for each element in an array and does not execute the functionn for
 empty elements.
 """
+import math
+
+def area(x):
+    return round(math.pi*(x**2), 2)
+
+radius = [1, 2, 3]
+
+area_list = list(map(area, radius))
+print(area_list)
+
+#prints [3.14, 12.57, 28.27]
