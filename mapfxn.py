a28333c09fd36ffe0bbc45b5ee93e221cbfb5ac2
jayjaychukwu <liljaydon.pj@gmail.com>
2022-06-21 08:58:18 +0100

    New file

---
+++ mapfxn.py
@@ -0,0 +1,6 @@
+"""
+map() is a function in python that creates a new array from
+calling a function for every array element. It calls a function once
+for each element in an array and does not execute the functionn for
+empty elements.
+"""
