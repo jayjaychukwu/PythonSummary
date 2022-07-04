b442a9a8231e7d15968f7d22a844e037ba33f126
jayjaychukwu <liljaydon.pj@gmail.com>
2022-07-04 16:55:29 +0100

    More on Ternary

--- ternary_operator.py
+++ ternary_operator.py
@@ -1,4 +1,13 @@
 """
 What is the ternary operator?
-
+It allows testing a condition on a single line 
 """
+
+#Syntax: [on_true] if [expression] else [on_false]
+
+a, b = 10, 20
+
+min = a if a < b else b
+
+print(min)
+#Output: 10
