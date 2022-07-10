93f77829624d88e0ea61040c2a5ec8ae4145e2a4
jayjaychukwu <liljaydon.pj@gmail.com>
2022-07-10 20:24:40 +0100

    Finished Ternary operator

--- ternary_operator.py
+++ ternary_operator.py
@@ -3,11 +3,33 @@ What is the ternary operator?
 It allows testing a condition on a single line 
 """
 
-#Syntax: [on_true] if [expression] else [on_false]
+# Syntax: [on_true] if [expression] else [on_false]
 
 a, b = 10, 20
 
 min = a if a < b else b
 
 print(min)
-#Output: 10
+# Output: 10
+
+"""
+There is a more direct way by using Dictionary, Tuples & lambda
+"""
+# With Tuple
+x, y = 20, 40
+
+# (if_check_is_false, if_check_is_true) [check]
+print((y, x)[x > y])
+# output: 40
+
+# With Dictionary
+
+print({True: x, False: y}[x > y])
+# output: 40
+
+# With lambda
+
+f = lambda x: 1.1 * x if x > 100 else 1.05 * x
+
+print(f(100))
+# Output: 105.0
